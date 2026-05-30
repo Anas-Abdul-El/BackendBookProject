055256829860adb82c2f70e5a11108b0e2827fa2
Anas-Abdul-El <anasabdulel@gmail.com>
2026-05-30 21:34:55 +0300

    I forget what is this

--- BackendBookBroject/obj/Debug/net10.0/BackendBookBroject.AssemblyInfo.cs
+++ BackendBookBroject/obj/Debug/net10.0/BackendBookBroject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendBookBroject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a60a8ef6485bf63000d077f89749f1d5ae937f2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c006e31169276cd7326b21ba19aa8210559066a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendBookBroject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendBookBroject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
